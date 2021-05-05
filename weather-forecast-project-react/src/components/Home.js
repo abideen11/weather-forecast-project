1602c84b62e6a57657d9d34f527ac4cc203b6203
Abideen Kareem <abideenkareem17@gmail.com>
2021-05-05 10:45:48 -0600

    add

--- weather-forecast-project-react/src/components/Home.js
+++ weather-forecast-project-react/src/components/Home.js
@@ -1,17 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Home() {
-    const [textInput, setTextInput] = useState("")
+    const [textInput, setTextInput] = useState("London")
+    const [weatherDetail, setWeatherDetail] = useState([])
+    useEffect(() => {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${textInput}&appid=${"9ebc44b6d29b8cbeef268e926871ff92"}`)
+        .then(r => r.json())
+        .then((data) => {
+            setWeatherDetail(data)
+        })
+    })
     const onInput = e => {
-        setTextInput(e.target.value)
+        if(e.target.value.length > 0) {
+            setTextInput(e.target.value)
+        }
+        else {
+            setTextInput("London")
+        }
     }
-    console.log(textInput)
+    console.log(textInput,"text")
+    console.log(weatherDetail,"weatherDetail19")
     return(
         <div className="div-hme">
             <div className="hme-div">
                 <h1>Home Page</h1>
                 <div className="dv-hme">
-                    {/* <div className="hme-d1">Your City</div> */}
                      <div className="hme-d2">
                        <form>
                             <label>
@@ -19,7 +32,8 @@ export default function Home() {
                                 <input type="text" value={textInput} onChange={onInput} />
                             </label>
                         </form>
-                    </div> 
+                    </div>
+                    <div></div> 
                 </div>
             </div>
         </div>
